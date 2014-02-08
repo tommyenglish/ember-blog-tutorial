3c1037df0f5d54f095eadffd0701c816cdbb4dfa
tommyenglish <t.english@ldschurch.org>
2014-02-07 17:06:06 -0700

    Step 6. Detailed post information.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ App = Ember.Application.create();
 App.Router.map(function() {
 	this.resource('about');
 	this.resource('posts');
+	this.resource('post', { path: ':post_id' });
 });
 
 var posts = [{
